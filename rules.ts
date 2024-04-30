30834d81441cefcf0652d8949b436a4bf2fb85df
Deshi Rahim <nawalsrahim@gmail.com>
2024-04-30 08:49:27 +0600

    ♻️ Add my own configruation

--- rules.ts
+++ rules.ts
@@ -1,24 +1,37 @@
-import fs from "fs";
-import { KarabinerRules } from "./types";
-import { createHyperSubLayers, app, open, rectangle } from "./utils";
+import fs from 'fs'
+import { KarabinerRules, KarabinerSimpleModifications } from './types'
+import { app, createHyperSubLayers, open } from './utils'
+
+const simpleModifications: KarabinerSimpleModifications[] = [
+  {
+    from: {
+      key_code: 'right_command',
+    },
+    to: [
+      {
+        key_code: 'escape',
+      },
+    ],
+  },
+]
 
 const rules: KarabinerRules[] = [
   // Define the Hyper key itself
   {
-    description: "Hyper Key (⌃⌥⇧⌘)",
+    description: 'Hyper Key (⌃⌥⇧⌘)',
     manipulators: [
       {
-        description: "Caps Lock -> Hyper Key",
+        description: 'Caps Lock -> Hyper Key',
         from: {
-          key_code: "caps_lock",
+          key_code: 'caps_lock',
           modifiers: {
-            optional: ["any"],
+            optional: ['any'],
           },
         },
         to: [
           {
             set_variable: {
-              name: "hyper",
+              name: 'hyper',
               value: 1,
             },
           },
@@ -26,203 +39,88 @@ const rules: KarabinerRules[] = [
         to_after_key_up: [
           {
             set_variable: {
-              name: "hyper",
+              name: 'hyper',
               value: 0,
             },
           },
         ],
         to_if_alone: [
           {
-            key_code: "escape",
+            key_code: 'escape',
           },
         ],
-        type: "basic",
+        type: 'basic',
       },
-      //      {
-      //        type: "basic",
-      //        description: "Disable CMD + Tab to force Hyper Key usage",
-      //        from: {
-      //          key_code: "tab",
-      //          modifiers: {
-      //            mandatory: ["left_command"],
-      //          },
-      //        },
-      //        to: [
-      //          {
-      //            key_code: "tab",
-      //          },
-      //        ],
-      //      },
     ],
   },
   ...createHyperSubLayers({
-    spacebar: open(
-      "raycast://extensions/stellate/mxstbr-commands/create-notion-todo"
-    ),
     // b = "B"rowse
-    b: {
-      t: open("https://twitter.com"),
-      // Quarterly "P"lan
-      p: open("https://qrtr.ly/plan"),
-      y: open("https://news.ycombinator.com"),
-      f: open("https://facebook.com"),
-      r: open("https://reddit.com"),
-    },
+    b: {},
     // o = "Open" applications
     o: {
-      1: app("1Password"),
-      g: app("Google Chrome"),
-      c: app("Notion Calendar"),
-      v: app("Visual Studio Code"),
-      d: app("Discord"),
-      s: app("Slack"),
-      e: app("Superhuman"),
-      n: app("Notion"),
-      t: app("Terminal"),
+      1: app('1Password'),
+      a: app('Arc'),
+      c: app('Notion Calendar'),
+      v: app('Visual Studio Code'),
+      d: app('Discord'),
+      s: app('Slack'),
+      n: app('Notion'),
+      t: app('Terminal'),
       // Open todo list managed via *H*ypersonic
       h: open(
-        "notion://www.notion.so/stellatehq/7b33b924746647499d906c55f89d5026"
+        'notion://www.notion.so/stellatehq/7b33b924746647499d906c55f89d5026'
       ),
-      z: app("zoom.us"),
-      // "M"essages
-      m: app("Texts"),
-      f: app("Finder"),
-      r: app("Texts"),
-      // "i"Message
-      i: app("Texts"),
-      p: app("Spotify"),
-      a: app("iA Presenter"),
-      // "W"hatsApp has been replaced by Texts
-      w: open("Texts"),
-      l: open(
-        "raycast://extensions/stellate/mxstbr-commands/open-mxs-is-shortlink"
-      ),
+      w: app('Warp'),
     },
 
-    // w = "Window" via rectangle.app
-    w: {
-      semicolon: {
-        description: "Window: Hide",
-        to: [
-          {
-            key_code: "h",
-            modifiers: ["right_command"],
-          },
-        ],
-      },
-      y: rectangle("previous-display"),
-      o: rectangle("next-display"),
-      k: rectangle("top-half"),
-      j: rectangle("bottom-half"),
-      h: rectangle("left-half"),
-      l: rectangle("right-half"),
-      f: rectangle("maximize"),
-      u: {
-        description: "Window: Previous Tab",
-        to: [
-          {
-            key_code: "tab",
-            modifiers: ["right_control", "right_shift"],
-          },
-        ],
-      },
-      i: {
-        description: "Window: Next Tab",
-        to: [
-          {
-            key_code: "tab",
-            modifiers: ["right_control"],
-          },
-        ],
-      },
-      n: {
-        description: "Window: Next Window",
-        to: [
-          {
-            key_code: "grave_accent_and_tilde",
-            modifiers: ["right_command"],
-          },
-        ],
-      },
-      b: {
-        description: "Window: Back",
-        to: [
-          {
-            key_code: "open_bracket",
-            modifiers: ["right_command"],
-          },
-        ],
-      },
-      // Note: No literal connection. Both f and n are already taken.
-      m: {
-        description: "Window: Forward",
-        to: [
-          {
-            key_code: "close_bracket",
-            modifiers: ["right_command"],
-          },
-        ],
-      },
-      d: {
-        description: "Window: Next display",
-        to: [
-          {
-            key_code: "right_arrow",
-            modifiers: ["right_control", "right_option", "right_command"],
-          },
-        ],
-      },
-    },
-
-    // s = "System"
     s: {
       u: {
         to: [
           {
-            key_code: "volume_increment",
+            key_code: 'volume_increment',
           },
         ],
       },
       j: {
         to: [
           {
-            key_code: "volume_decrement",
+            key_code: 'volume_decrement',
           },
         ],
       },
       i: {
         to: [
           {
-            key_code: "display_brightness_increment",
+            key_code: 'display_brightness_increment',
           },
         ],
       },
       k: {
         to: [
           {
-            key_code: "display_brightness_decrement",
+            key_code: 'display_brightness_decrement',
           },
         ],
       },
       l: {
         to: [
           {
-            key_code: "q",
-            modifiers: ["right_control", "right_command"],
+            key_code: 'q',
+            modifiers: ['right_control', 'right_command'],
           },
         ],
       },
       p: {
         to: [
           {
-            key_code: "play_or_pause",
+            key_code: 'play_or_pause',
           },
         ],
       },
       semicolon: {
         to: [
           {
-            key_code: "fastforward",
+            key_code: 'fastforward',
           },
         ],
       },
@@ -230,23 +128,8 @@ const rules: KarabinerRules[] = [
         to: [
           {
             // Emoji picker
-            key_code: "spacebar",
-            modifiers: ["right_control", "right_command"],
-          },
-        ],
-      },
-      // Turn on Elgato KeyLight
-      y: {
-        to: [
-          {
-            shell_command: `curl -H 'Content-Type: application/json' --request PUT --data '{ "numberOfLights": 1, "lights": [ { "on": 1, "brightness": 100, "temperature": 215 } ] }' http://192.168.8.84:9123/elgato/lights`,
-          },
-        ],
-      },
-      h: {
-        to: [
-          {
-            shell_command: `curl -H 'Content-Type: application/json' --request PUT --data '{ "numberOfLights": 1, "lights": [ { "on": 0, "brightness": 100, "temperature": 215 } ] }' http://192.168.8.84:9123/elgato/lights`,
+            key_code: 'spacebar',
+            modifiers: ['right_control', 'right_command'],
           },
         ],
       },
@@ -258,77 +141,65 @@ const rules: KarabinerRules[] = [
     // so that hjkl work like they do in vim
     v: {
       h: {
-        to: [{ key_code: "left_arrow" }],
+        to: [{ key_code: 'left_arrow' }],
       },
       j: {
-        to: [{ key_code: "down_arrow" }],
+        to: [{ key_code: 'down_arrow' }],
       },
       k: {
-        to: [{ key_code: "up_arrow" }],
+        to: [{ key_code: 'up_arrow' }],
       },
       l: {
-        to: [{ key_code: "right_arrow" }],
+        to: [{ key_code: 'right_arrow' }],
       },
       // Magicmove via homerow.app
       m: {
-        to: [{ key_code: "f", modifiers: ["right_control"] }],
+        to: [{ key_code: 'f', modifiers: ['right_control'] }],
       },
       // Scroll mode via homerow.app
       s: {
-        to: [{ key_code: "j", modifiers: ["right_control"] }],
+        to: [{ key_code: 'j', modifiers: ['right_control'] }],
       },
       d: {
-        to: [{ key_code: "d", modifiers: ["right_shift", "right_command"] }],
+        to: [{ key_code: 'd', modifiers: ['right_shift', 'right_command'] }],
       },
       u: {
-        to: [{ key_code: "page_down" }],
+        to: [{ key_code: 'page_down' }],
       },
       i: {
-        to: [{ key_code: "page_up" }],
+        to: [{ key_code: 'page_up' }],
       },
     },
 
     // c = Musi*c* which isn't "m" because we want it to be on the left hand
     c: {
       p: {
-        to: [{ key_code: "play_or_pause" }],
+        to: [{ key_code: 'play_or_pause' }],
       },
       n: {
-        to: [{ key_code: "fastforward" }],
+        to: [{ key_code: 'fastforward' }],
       },
       b: {
-        to: [{ key_code: "rewind" }],
+        to: [{ key_code: 'rewind' }],
       },
     },
 
     // r = "Raycast"
     r: {
-      n: open("raycast://script-commands/dismiss-notifications"),
-      l: open(
-        "raycast://extensions/stellate/mxstbr-commands/create-mxs-is-shortlink"
-      ),
+      n: open('raycast://script-commands/dismiss-notifications'),
       e: open(
-        "raycast://extensions/raycast/emoji-symbols/search-emoji-symbols"
+        'raycast://extensions/raycast/emoji-symbols/search-emoji-symbols'
       ),
-      c: open("raycast://extensions/raycast/system/open-camera"),
-      p: open("raycast://extensions/raycast/raycast/confetti"),
-      a: open("raycast://extensions/raycast/raycast-ai/ai-chat"),
-      s: open("raycast://extensions/peduarte/silent-mention/index"),
-      h: open(
-        "raycast://extensions/raycast/clipboard-history/clipboard-history"
-      ),
-      1: open(
-        "raycast://extensions/VladCuciureanu/toothpick/connect-favorite-device-1"
-      ),
-      2: open(
-        "raycast://extensions/VladCuciureanu/toothpick/connect-favorite-device-2"
-      ),
+      c: open('raycast://extensions/raycast/system/open-camera'),
+      p: open('raycast://extensions/raycast/raycast/confetti'),
+      a: open('raycast://extensions/raycast/raycast-ai/ai-chat'),
+      s: open('raycast://extensions/peduarte/silent-mention/index'),
     },
   }),
-];
+]
 
 fs.writeFileSync(
-  "karabiner.json",
+  'karabiner.json',
   JSON.stringify(
     {
       global: {
@@ -336,7 +207,8 @@ fs.writeFileSync(
       },
       profiles: [
         {
-          name: "Default",
+          name: 'Default',
+          simple_modifications: simpleModifications,
           complex_modifications: {
             rules,
           },
@@ -346,4 +218,4 @@ fs.writeFileSync(
     null,
     2
   )
-);
+)
